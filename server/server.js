c80e302c64f68e48ae115e017e7afd493c1bd35c
amboob223 <monk8896@gmail.com>
2023-10-23 03:44:46 -0400

    ss

---
+++ server/server.js
@@ -0,0 +1,122 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const pool = require("./db");
+const multer = require("multer");
+const path = require("path");
+const brcrypt = require("bcrypt");
+
+//middleware
+app.use(express.json());
+app.use(cors());
+app.set("view engine", "ejs")
+app.use(express.static('server/images'))
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "images/")
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname)) // original file bame is the file name on the xonmputer
+
+    }
+});
+
+const upload = multer({
+    storage: storage
+}); // this is a multer instance where we use the multer object and thestorage property
+
+//signup
+app.post("/signup", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const saltRounds = 10
+        const hashedword = await brcrypt.hash(password, saltRounds)// we using await because we reaching out to a diffdrent tech 
+        const data = await pool.query(
+            "INSERT INTO pass(email,password) VALUES($1,$2) RETURNING *",
+            [email, hashedword]
+        );
+        res.json(data.rows[0])
+    } catch (error) {
+        console.log(error)
+        res.status(400).json
+    }
+});
+
+app.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body
+        const data = await pool.query(
+            "SELECT password FROM pass WHERE email = $1")
+
+        if (data.rows.length === 1) {
+            const hashedPassword = data.rows[0].password;
+            const passwordMatch = await bcrypt.compare(password, hashedPassword)
+
+            if (passwordMatch) {
+                res.status(200).json({ message: "login succesful" })
+            } else {
+                res.status(401).json({ error: "invalid" })
+            }
+
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500)
+    }
+})
+app.post("/promo", async (req, res) => {
+    try {
+        const { namee, sections, phone, email, club, cost, description } = req.body
+        const pic = req.file.filename
+        const data = await pool.query(
+            "INSERT INTO promo(name,sections,club,phone,date,cost,pic,description) VALUES ($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *",
+            [namee, sections, phone, email, club, cost, pic, description]
+        );
+        res.json(data.rows[0])
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ error: "whats wrong" })
+    }
+});
+
+app.get("/promo", async (req, res) => {
+    try {
+        const data = await pool.query(
+            "SELECT * FROM promo;"
+        );
+        res.json(data.rows)
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+app.post("/user", async (req, res) => {
+    try {
+        const { namee, phone, email } = req.body
+
+        const data = await pool.query(
+            "INSERT INTO promo(name,club,phone,date,cost,pic,description) VALUES ($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *",
+            [namee, phone, email]
+        );
+        res.json(data.rows[0])
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ error: "whats wrong" })
+    }
+});
+
+app.get("/user", async (req, res) => {
+    try {
+        const data = await pool.query(
+            "SELECT * FROM user;"
+        );
+        res.json(data.rows)
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+app.listen(5000, () => {
+    console.log("cool")
+})
---
+++ server/sql.sql
@@ -0,0 +1,33 @@
+-- Create the 'club' database
+CREATE DATABASE club;
+
+-- Use the 'club' database
+\c club;
+
+-- Create the 'signup' table
+CREATE TABLE pass(
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255), -- 'user' is a reserved keyword, so it should be enclosed in double quotes
+    password VARCHAR(255)
+)
+
+-- Create the 'promo' table
+CREATE TABLE promo(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    sections VARCHAR(255),
+    club VARCHAR(255),
+    phone VARCHAR(255),
+    date VARCHAR(255),
+    cost VARCHAR(255),
+    pic VARCHAR(255), -- Corrected 'bytea' data type
+    description VARCHAR(255)
+);
+
+-- Create the 'user' table
+CREATE TABLE "user"(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    phone VARCHAR(255),
+    email VARCHAR(255)
+);
